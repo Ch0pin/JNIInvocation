87ee207c3045cd1747eac1d9e078484cb010b3f9
Ch0pin <ch0pin@mackis.home>
2023-08-28 16:05:21 +0100

    removed unnecessary files

--- build.sh
+++
@@ -1,7 +0,0 @@
-rm -r build/
-
-mkdir build && cd build
-cmake -DANDROID_PLATFORM=31 \
-        -DCMAKE_TOOLCHAIN_FILE=$HOME/Library/Android/sdk/ndk/25.2.9519653/build/cmake/android.toolchain.cmake \
-        -DANDROID_ABI=arm64-v8a ..
-make
